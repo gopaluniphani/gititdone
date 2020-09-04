4f3dfccdd4b6f9e6891f649f4d6f5a70c3e4ce9b
Phani Gopaluni <gopaluniphani@gmail.com>
2020-09-04 21:03:46 +0530

    added buttons in chatbot

--- src/components/ChatBotWidget.js
+++ src/components/ChatBotWidget.js
@@ -5,7 +5,8 @@ import { Widget } from 'rasa-webchat';
 const ChatBotWidget = () => {
   return (
     <Widget
-      socketUrl={"https://getitdonebot.eastasia.cloudapp.azure.com"}
+      initPayload={"/get_started"}
+      socketUrl={"http://getitdone.centralindia.cloudapp.azure.com:5005"}
       socketPath={"/socket.io/"}
       title={"GetItDone"}
       customData={{"language": "en"}}
